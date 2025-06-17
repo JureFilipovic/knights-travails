9e6cc3e3f34f69e55fb0ad79bce71e540db8ec86
JureFilipovic <jure.filipovic@gmail.com>
2025-06-17 12:48:20 +0200

    Testing.

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import knightMoves from "./knight-moves.js";
+
+console.log(knightMoves([0, 0], [3, 3]));
+
+console.log(knightMoves([3, 3], [7, 7]));
+
+console.log(knightMoves([4, 4], [4, 4]));
+
+console.log(knightMoves([0, 0], [7, 7]));
+
+console.log(knightMoves([7, 7], [0, 0]));
+
+console.log(knightMoves([3, 3], [5, 4]));

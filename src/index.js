17fe0dd1906d5a8613218c14dadaaacaecae7e75
JureFilipovic <jure.filipovic@gmail.com>
2025-06-17 12:53:11 +0200

    More testing.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import knightMoves from "./knight-moves.js";
 
-console.log(knightMoves([0, 0], [3, 3]));
+const path = knightMoves([3,3],[4,3]);
 
-console.log(knightMoves([3, 3], [7, 7]));
-
-console.log(knightMoves([4, 4], [4, 4]));
-
-console.log(knightMoves([0, 0], [7, 7]));
-
-console.log(knightMoves([7, 7], [0, 0]));
-
-console.log(knightMoves([3, 3], [5, 4]));
+console.log(`You made it in ${path.length - 1} moves! Here is your path:`);
+path.forEach(move => {
+    console.log(`[${move[0]}, ${move[1]}]`);
+});

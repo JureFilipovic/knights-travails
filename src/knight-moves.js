3327af3bf4af3b8da1806fdd27c4fd8061e36165
JureFilipovic <jure.filipovic@gmail.com>
2025-06-17 12:47:11 +0200

    Implement knightMoves function using BFS to find shortest path on chessboard
    - Uses precomputed graph of knight moves from createGraph()
    - Tracks visited positions to avoid cycles
    - Stores paths in queue to reconstruct shortest path upon reaching target
    - Returns array of positions from start to target representing shortest knight path

---
+++ src/knight-moves.js
@@ -0,0 +1,34 @@
+import createGraph from "./graph.js";
+import Vertex from "./vertex.js";
+
+const graph = createGraph();
+
+export default function knightMoves(start, target) {
+    const queue = [];
+    const visited = new Set();
+
+    const positionToKey = ([x, y]) => `${x},${y}`;
+
+    queue.push({ position: start, path: [start] });
+    visited.add(positionToKey(start));
+
+    while (queue.length > 0) {
+        const { position, path } = queue.shift();
+
+        if (position[0] === target[0] && position[1] === target[1]) {
+            return path;
+        }
+
+        const node = graph[position[0]][position[1]];
+        const moves = node.getMoves();
+
+        for (let move of moves) {
+            const key = positionToKey(move);
+
+            if (!visited.has(key)) {
+                visited.add(key);
+                queue.push({ position: move, path: [...path, move] });
+            }
+        }
+    }
+}
